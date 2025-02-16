401981c9114f5ba2af969e85b979c8af0b3be0a4
Nahid44332 <nahidhossen3278@gmail.com>
2025-02-16 05:00:32 -0800

    Js Array

---
+++ script.js
@@ -0,0 +1,20 @@
+let twoArray = [
+    [36, 27, 56, 33],
+    [4, 8, 12, 29],
+    [99, 86, 84, 76]
+];
+
+console.log(twoArray[0][0]);
+console.log(twoArray[0][1]);
+console.log(twoArray[0][2]);
+console.log(twoArray[0][3]);
+
+console.log(twoArray[1][0]);
+console.log(twoArray[1][1]);
+console.log(twoArray[1][2]);
+console.log(twoArray[1][3]);
+
+console.log(twoArray[2][0]);
+console.log(twoArray[2][1]);
+console.log(twoArray[2][2]);
+console.log(twoArray[2][3]);
